90ee107419eae12448ec591dac6ffd2e42168e95
Steven Persia <persia.steven8@gmail.com>
2019-01-08 13:20:32 +0100

    Build production

---
+++ build/precache-manifest.90a07c76df28e1f81dd49d0fccd9d551.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "1e577a68c6901c25fd6a73ee903cb773",
+    "url": "/static/media/dab2.1e577a68.gif"
+  },
+  {
+    "revision": "6f5f66211ac6411b6c3b",
+    "url": "/static/css/main.321a506e.chunk.css"
+  },
+  {
+    "revision": "37b539a2bab09f4c8a5b",
+    "url": "/static/js/1.37b539a2.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "8a25566d0430b5b12b77594f0c3964e4",
+    "url": "/static/media/puissance.8a25566d.mp3"
+  },
+  {
+    "revision": "6f5f66211ac6411b6c3b",
+    "url": "/static/js/main.6f5f6621.chunk.js"
+  },
+  {
+    "revision": "efe950d07399409e439e70f565f26a9d",
+    "url": "/static/media/jspower.efe950d0.eot"
+  },
+  {
+    "revision": "1e3c5a01763dea40f54182a50b2e6e99",
+    "url": "/static/media/jspower.1e3c5a01.ttf"
+  },
+  {
+    "revision": "b271e8b33d56db99e27f0551debf23d4",
+    "url": "/static/media/jspower.b271e8b3.svg"
+  },
+  {
+    "revision": "d27873f1fdf5b1d42e8366dd335a975c",
+    "url": "/static/media/jspower.d27873f1.woff"
+  },
+  {
+    "revision": "1049e41c34b8de38b964d2aa7b6477b0",
+    "url": "/index.html"
+  }
+];
